0b0274ceed09d49d59c37df483fbabbf89c02153
AgatheArchet <agathe.archet@ensta-bretagne.org>
2021-02-03 11:52:38 +0100

    update aruco

--- localisation_aruco/src/aruco.cpp
+++ localisation_aruco/src/aruco.cpp
@@ -7,10 +7,10 @@
 #include "geometry_msgs/msg/pose.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "std_msgs/msg/header.hpp"
+#include <visualization_msgs/msg/marker.hpp>
+#include <visualization_msgs/msg/marker_array.hpp> 
 
 #include <opencv2/aruco.hpp>
-//#include "visualization_msgs/msg/Marker.hpp"
-//#include "visualization_msgs/msg/MarkerArray.hpp" 
 
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -45,33 +45,28 @@ class MinimalSubscriber : public rclcpp::Node
 		try
 		{
 			cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-			Mat frame = cv_ptr->image;
+			Mat frame = cv::imread("./test.png");//cv_ptr->image;
 			cv::Mat frameCopy;
     		frame.copyTo(frameCopy);
 			
-   //  		// detection du marker 0
-			// float x_t, y_t, z_t;
-		 //    long int index;
-		 //    float roll,yaw,pitch;
-		 //    std::vector<cv::Vec3d> rvecs, tvecs;
-/*
+    		// detection du marker 0
+			float x_t, y_t, z_t;
+		    long int index;
+		    float roll,yaw,pitch;
+		    std::vector<cv::Vec3d> rvecs, tvecs;
+
 		    cv::aruco::detectMarkers(frame, dictionary, markerCorners, markerIds);
-		    visualization_msgs::MarkerArray marker_array_msg;
-		    marker_array_msg.markers.resize(2);
+		    cout <<"test : "<<markerCorners.size() << endl;
+		    visualization_msgs::msg::MarkerArray marker_array_msg;
 
-		    for(int i =0; i < marker_array_msg.markers.size(); i++){
-		            marker_array_msg.markers[i].header.frame_id = "camera";
-		            marker_array_msg.markers[i].header.stamp = ros::Time();
-		            marker_array_msg.markers[i].id = -1;
-		            marker_array_msg.markers[i].lifetime = ros::Duration();
-		    }
-
-		    // tracé des arucos sur l'image
 		    if (markerIds.size() > 0) {
-      			  cv::aruco::drawDetectedMarkers(frameCopy, markerCorners, markerIds,cv::Scalar(255, 0, 0));
+      			  cv::aruco::drawDetectedMarkers(frame, markerCorners, markerIds,cv::Scalar(255, 0, 0));
+      			  cout << "ok" << endl;
+      			  imshow("Entrée",frame);
       		}
-*/			
-			imshow("Entrée",frame);
+			else{
+				imshow("Entrée",frameCopy);
+			}
 			waitKey(3);
 
 		}
