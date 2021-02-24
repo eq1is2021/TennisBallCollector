c44cedd1b43faec43992cd42d2752c2f7180c920
EdouardSaladier <alexandre.evain76@gmail.com>
2021-02-24 08:37:08 +0100

    Le robot ignore les balles près du mur

--- robot_control/robot_control/rbt_control.py
+++ robot_control/robot_control/rbt_control.py
@@ -16,6 +16,7 @@ terrain_x=30
 terrain_y=16
 wall_center=15
 wall_length=11.15
+len_space=terrain_y-wall_center-wall_length/2
 goal_zone_left_x=2.7
 goal_zone_left_y=terrain_y-2.5
 goal_zone_right_x=terrain_x-2.7
@@ -34,7 +35,6 @@ def get_dist(pos_robot,pos_target):
 
 #Fonction renvoyant la position du passage du mur le plus proche du robot
 def get_passage_wall(pos_robot):
-    len_space=terrain_y-wall_center-wall_length/2
     if pos_robot[1]<terrain_y/2:
         return(terrain_x/2,len_space/2)
     else:
@@ -92,7 +92,7 @@ def test_goal(pos_robot):
     dist_right=np.linalg.norm([x_diff_right, y_diff_right])
     # print("dist_left: ", dist_left)
     # print("dist_right: ", dist_right)
-    if dist_left < 2 or dist_right < 2:
+    if dist_left < 1 or dist_right < 1:
         return True
     else:
         return False
@@ -106,6 +106,17 @@ def test_ball_near(pos_robot,pos_balle,d):
         return True
     else:
         return False
+
+def test_ball_wall(pos_balle):
+    #Fonction de test pour voir si une balle est proche du mur. Return True si la balle est loin du mur et on peut la prendre, False si elle est proche du mur
+    d_t=0.2
+    if pos_balle[0]<d_t or pos_balle[0]>terrain_x-d_t:
+        return False
+    if pos_balle[1]<d_t or pos_balle[1]>terrain_y-d_t:
+        return False
+    if (wall_center-d_t<pos_balle[0]<wall_center+d_t) and (len_space-d_t<pos_balle[1]<len_space+wall_length+d_t):
+        return False
+    return True
 
 # def test_choppe_balle(pos_robot,pos_balle):
 #     x_diff=pos_balle[0]-pos_robot[0]
@@ -178,7 +189,7 @@ class Robot_Control(Node):
             self.pos_balle[i,0]=msg.poses[i].position.x
             self.pos_balle[i,1]=msg.poses[i].position.y
             self.pos_balle[i,2]=msg.poses[i].position.z
-            if msg.poses[i].position.z > 0.5:
+            if msg.poses[i].position.z > 0.5 :
                 # Si la balle existe, on la compte
                 self.num_balle += 1
                 # print(self.num_balle)
@@ -197,7 +208,7 @@ class Robot_Control(Node):
         for i in range(10):
             # Si la balle existe
             # print(self.pos_balle[i][2])
-            if self.pos_balle[i][2]==1:
+            if self.pos_balle[i][2]==1 and test_ball_wall(pos_balle):
                 # print("testing ball: ",i)
                 # Poids de l'id
                 weight_id = ((10-i)/10)*2/3
